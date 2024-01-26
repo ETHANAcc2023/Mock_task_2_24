246b224189770f77d02ce3e5e64e493b21212c01
EthanAcc2023 <ethan.hurkett.754@accesstomusic.ac.uk>
2024-01-26 12:28:21 +0000

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-        extend: {
-      colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            'tan': '#F0F0DC',
-            'brown': '#916A34',
-            'blue': '#429EC8'
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+        colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'tan': '#F0F0DC',
+                'brown': '#916A34',
+                'blue': '#429EC8'
         },
-    }
-    },
+      }
+  },
   }
+  
